34a1c2ede49b494302d044066818b6fb5e2af13b
ElricM <elric.milton@yahoo.com.au>
2021-02-20 09:36:59 +1100

    Added collect script

--- 20022021 Gamejam/Assets/Scripts/ChildOnCollision.cs
+++ 20022021 Gamejam/Assets/Scripts/ChildOnCollision.cs
@@ -17,6 +17,15 @@ public class ChildOnCollision : MonoBehaviour
     }
     private void OnCollisionEnter(Collision collision)
     {
-
+        if (collision.collider.CompareTag("Collectable"))
+        {
+            ContactPoint contact = collision.contacts[0];
+            Vector3 pos = contact.point;
+            GameObject ob = collision.gameObject;
+            ob.transform.parent = gameObject.transform;
+            ob.transform.position = pos;
+            ob.GetComponent<Collider>().enabled = false;
+            //collision.gameObject.transform.localPosition
+        }
     }
 }
