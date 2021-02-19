6631461bdf2f1e0e2aec34774f1a0dd71eb40fef
ElricM <elric.milton@yahoo.com.au>
2021-02-20 09:03:10 +1100

    added a script

---
+++ 20022021 Gamejam/Assets/Scripts/ChildOnCollision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildOnCollision : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+
+    }
+}
